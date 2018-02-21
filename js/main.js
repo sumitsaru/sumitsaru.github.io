7108305e81714b81433e2730a30ea79798001cae
sumit kumar <sumit_jaiswal18@live.com>
2018-02-20 19:58:32 -0500

    javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-console.log('welcome')
+console.log('welcome');
+
+// set a cookie with their name in it
+
+Cookies.set('firstName', 'sumit')
+
+// on click of button,
+document.getElementById('button').addEventListener('click', function() {
+  alert('baby on board')
+    // get the .value of click button
+    var fName = document.getElementById('fname').value;
+    console.log('fName')
+});
+// get the .value of fname
+// store that value in a cookiesExport
